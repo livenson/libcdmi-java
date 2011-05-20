66c05a86f92804a0b35eab642ce883ebd0361a83
Ali Gholami <gholami@tb06.pdc.kth.se>
2011-05-20 12:03:28 +0200

    Non-cdmi operation testcases.

---
+++ src/test/java/eu/venusc/cdmi/NCDMIBlobOperationsTest.java
@@ -0,0 +1,139 @@
+package eu.venusc.cdmi;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Random;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NCDMIBlobOperationsTest extends CDMIConnectionWrapper implements
+		CDMIResponseStatus {
+
+	NCDMIContainerOperations cops = null;
+	NCDMIBlobOperations bops = null;
+	static String containerName = null;
+	static String baseContainer = null;
+	static String objectName = null;
+	static Random random = new Random();
+	static File tmpFile = null;
+
+	public NCDMIBlobOperationsTest(String name) throws MalformedURLException {
+		super(name);
+		cops = cdmiConnection.getNcdmiContainerProxy();
+		bops = cdmiConnection.getNcdmiBlobProxy();
+	}
+
+	@Before
+	public void setUp() throws IOException {		
+		baseContainer = "/";
+		if (baseContainer.charAt(baseContainer.length()-1)!='/')
+			baseContainer = baseContainer + "/";
+		
+		containerName = "noncdmi-container" + random.nextInt();
+		tmpFile = Utils.createFile("put your data here", "venus_c", ".txt");
+		objectName = tmpFile.getName();		
+	}
+
+
+	@After
+	public void tearDown() throws IOException, CDMIOperationException {
+
+		HttpResponse response = response = bops.delete(baseContainer+ containerName + "/" + objectName);
+		int responseCode = response.getStatusLine().getStatusCode();
+		// we only accept failures for already deleted objects
+		if (responseCode != REQUEST_DELETED && responseCode != REQUEST_NOT_FOUND) {		
+			fail("Could not clean the object: " + baseContainer+ containerName + "/" + objectName);
+		}
+		response = cops.delete(baseContainer+ containerName);
+		responseCode = response.getStatusLine().getStatusCode();
+		if (responseCode != REQUEST_DELETED)
+			fail("Could not clean up the container: " + baseContainer+ containerName + "/");
+	}
+
+
+	
+	@Test
+	public void testCreate() throws ClientProtocolException, IOException,
+			CDMIOperationException {
+
+		HttpResponse  response = cops.create(baseContainer+ containerName, parameters);
+		int  responseCode = response.getStatusLine().getStatusCode();
+
+		if (responseCode != REQUEST_CREATED)
+			fail("Could not create container: "+ baseContainer+ containerName+"/" + responseCode);
+
+		response = bops.create(baseContainer+ containerName + "/" + objectName, Utils
+				.getBytesFromFile(tmpFile), parameters);
+		responseCode = response.getStatusLine().getStatusCode();
+		assertEquals("Object could not be created: "+baseContainer+ containerName + "/" + objectName, REQUEST_CREATED, responseCode);
+
+	}
+
+	@Test
+	public void testRead() throws IOException, CDMIOperationException,
+			ParseException {		
+	
+		HttpResponse response = cops.create(baseContainer+ containerName , parameters);
+	
+		int responseCode = response.getStatusLine().getStatusCode();	
+		if (responseCode != REQUEST_CREATED)
+			fail("Could not create container: " + baseContainer+ containerName + "/");
+
+		response = bops.create(baseContainer+ containerName + "/" + objectName, Utils
+				.getBytesFromFile(tmpFile), parameters);
+
+		responseCode = response.getStatusLine().getStatusCode();
+		
+		if (responseCode != REQUEST_CREATED)
+			fail("Could not create blob object " + baseContainer+ containerName + "/"
+					+ objectName);
+
+		response = bops.read(baseContainer+ containerName + "/" + objectName);
+		responseCode = response.getStatusLine().getStatusCode();
+		String mimeType = (String) Utils.getElement(response, "mimetype");
+
+		response = bops.read(baseContainer+ containerName + "/" + objectName);
+		responseCode = response.getStatusLine().getStatusCode();
+
+		if (!mimeType.equals("text/plain")) {
+			assertEquals("Local and remote blob objects equal: ", new String(
+					Utils.getBytesFromFile(tmpFile)), Utils
+					.extractContents(response));
+
+		} else {
+			assertEquals("Local and remote blob objects equal: ", new String(
+					Utils.getBytesFromFile(tmpFile)), new String(Utils
+					.extractContents(response)));
+		}
+
+	}
+
+
+	@Test
+	public void testDelete() throws IOException, CDMIOperationException {
+		HttpResponse response = cops.create(baseContainer+ containerName , parameters);
+		int responseCode = response.getStatusLine().getStatusCode();
+		if (responseCode != REQUEST_CREATED)
+			fail("Could not create container: " + baseContainer+ containerName + "/");
+
+		response = bops.create(baseContainer+ containerName + "/" + objectName, Utils
+				.getBytesFromFile(tmpFile), parameters);
+
+		responseCode = response.getStatusLine().getStatusCode();
+		if (responseCode != REQUEST_CREATED)
+			fail("Could not create blob object: " +baseContainer+ containerName + "/"
+					+ objectName);
+
+		response = bops.delete(baseContainer+ containerName + "/" + objectName);
+		responseCode = response.getStatusLine().getStatusCode();
+		assertEquals("Object deleted: ", REQUEST_DELETED, responseCode);
+
+	}
+
+}
---
+++ src/test/java/eu/venusc/cdmi/NCDMIContainerOperationsTest.java
@@ -0,0 +1,122 @@
+package eu.venusc.cdmi;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NCDMIContainerOperationsTest extends CDMIConnectionWrapper implements
+CDMIResponseStatus {
+
+	
+	NCDMIContainerOperations cops = null;
+	static String containerName = null;
+	static String baseContainer = null;
+	static Random random = new Random();
+
+	public NCDMIContainerOperationsTest(String name) throws MalformedURLException {
+		super(name);
+		cops = cdmiConnection.getNcdmiContainerProxy();
+	}
+
+	
+	@Before
+	public void setUp() throws Exception {
+		baseContainer = "/";
+		if (baseContainer.charAt(baseContainer.length()-1)!='/')
+			baseContainer = baseContainer + "/";
+		
+		containerName = "noncdmi-container" + random.nextInt();
+	}
+	@After
+	public void tearDown() throws IOException, ParseException,
+			CDMIOperationException {
+
+		String[] children = cops.getChildren(baseContainer);
+
+		for (int i = 0; i < children.length; i++) {
+			String url = baseContainer + children[i];
+			HttpResponse response = cops.delete(url);
+			int responseCode = response.getStatusLine().getStatusCode();
+
+			if (responseCode == REQUEST_NOT_FOUND
+					|| responseCode == REQUEST_DELETED)
+				continue;
+			else
+			fail("Container " + url + " could not be cleaned up." + responseCode);
+
+		}
+	}
+
+	@Test
+	public void testCreate() throws ClientProtocolException, IOException,
+			CDMIOperationException {
+		HttpResponse response = cops.create(baseContainer + containerName,
+				parameters);
+		int responseCode = response.getStatusLine().getStatusCode();
+		assertEquals("Creating container " + baseContainer + containerName
+				+ "/", REQUEST_CREATED, responseCode);
+	}
+
+	@Test
+	public void testGetChildren() throws ClientProtocolException, IOException,
+			CDMIOperationException, ParseException {
+
+		Set<String> set = new HashSet<String>();
+
+		// create containers
+		for (int i = 0; i < 3; i++) {
+
+			HttpResponse response = cops.create(baseContainer + containerName
+					+ i, parameters);
+			int responseCode = response.getStatusLine().getStatusCode();
+
+			if (responseCode != REQUEST_CREATED)
+				fail("Could not create  the container: " + baseContainer
+						+ containerName + "/");
+			set.add(containerName + i + "/");
+		}
+
+		String[] children = cops.getChildren(baseContainer);
+
+		Set<String> childSet = new HashSet<String>();
+		for (int i = 0; i < children.length; i++) {
+			childSet.add(children[i]+ "/");
+		}
+
+		assertEquals("Getting the container children: ", set, childSet);
+		childSet.clear();
+
+	}
+
+	@Test
+	public void testDelete() throws ClientProtocolException, IOException,
+			CDMIOperationException {
+
+		HttpResponse response = null;
+		int responseCode = 0;
+
+		// Create a container
+		response = cops.create(baseContainer + containerName, parameters);
+		responseCode = response.getStatusLine().getStatusCode();
+
+		if (responseCode != REQUEST_CREATED)
+			fail("Could not create the container: " + baseContainer
+					+ containerName);
+		// delete the container
+		response = cops.delete(baseContainer + containerName);
+		responseCode = response.getStatusLine().getStatusCode();
+		assertEquals("Container deleted: " + baseContainer + containerName,
+				REQUEST_DELETED, responseCode);
+	}
+}
