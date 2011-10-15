d5f776f5aa1e8b647ae22f8b24703f44d4c8f5ba
livenson <ilja.livenson@gmail.com>
2011-10-15 09:45:20 +0300

    minor code formatting

--- src/main/java/eu/venusc/cdmi/CDMIConnection.java
+++ src/main/java/eu/venusc/cdmi/CDMIConnection.java
@@ -1,10 +1,6 @@
 package eu.venusc.cdmi;
 
-import static eu.venusc.cdmi.CDMIResponseStatus.REQUEST_OK;
-
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
@@ -14,11 +10,9 @@ import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 
 import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
-import org.apache.http.client.HttpClient;
 import org.apache.http.conn.routing.HttpRoute;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
@@ -30,113 +24,112 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.HTTP;
-import org.json.simple.parser.ParseException;
 
 public class CDMIConnection {
 
-	private DefaultHttpClient httpclient;
-	private BlobOperations blobProxy;
-	private ContainerOperations containerProxy;
-	private NonCDMIContainerOperations nonCdmiContainerProxy;
-	private NonCDMIBlobOperations nonCdmiBlobProxy;
-	private URL endpoint;
+    private DefaultHttpClient httpclient;
+    private BlobOperations blobProxy;
+    private ContainerOperations containerProxy;
+    private NonCDMIContainerOperations nonCdmiContainerProxy;
+    private NonCDMIBlobOperations nonCdmiBlobProxy;
+    private URL endpoint;
 
-	public NonCDMIBlobOperations getNonCdmiBlobProxy() {
-		return nonCdmiBlobProxy;
-	}
+    public NonCDMIBlobOperations getNonCdmiBlobProxy() {
+        return nonCdmiBlobProxy;
+    }
 
-	public void setNonCdmiBlobProxy(NonCDMIBlobOperations ncdmiBlobProxy) {
-		this.nonCdmiBlobProxy = ncdmiBlobProxy;
-	}
+    public void setNonCdmiBlobProxy(NonCDMIBlobOperations ncdmiBlobProxy) {
+        this.nonCdmiBlobProxy = ncdmiBlobProxy;
+    }
 
-	public NonCDMIContainerOperations getNonCdmiContainerProxy() {
-		return nonCdmiContainerProxy;
-	}
+    public NonCDMIContainerOperations getNonCdmiContainerProxy() {
+        return nonCdmiContainerProxy;
+    }
 
-	public void setNonCdmiContainerProxy(
-			NonCDMIContainerOperations nonCdmiContainerProxy) {
-		this.nonCdmiContainerProxy = nonCdmiContainerProxy;
-	}
+    public void setNonCdmiContainerProxy(
+            NonCDMIContainerOperations nonCdmiContainerProxy) {
+        this.nonCdmiContainerProxy = nonCdmiContainerProxy;
+    }
 
-	public CDMIConnection(Credentials credentials, URL endpoint)
-			throws CertificateException, NoSuchAlgorithmException,
-			KeyManagementException, IOException, KeyStoreException,
-			UnrecoverableKeyException {
+    public CDMIConnection(Credentials credentials, URL endpoint)
+            throws CertificateException, NoSuchAlgorithmException,
+            KeyManagementException, IOException, KeyStoreException,
+            UnrecoverableKeyException {
 
-		KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-		// TODO: load server credentials
-		trustStore.load(null, null);
+        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        // TODO: load server credentials
+        trustStore.load(null, null);
 
-		SSLSocketFactory blindTrustFactory = new CustomSSLSocketFactory(
-				trustStore);
-		blindTrustFactory
-				.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+        SSLSocketFactory blindTrustFactory = new CustomSSLSocketFactory(
+                trustStore);
+        blindTrustFactory
+                .setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
 
-		HttpParams params = new BasicHttpParams();
-		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-		HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+        HttpParams params = new BasicHttpParams();
+        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+        HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
 
-		SchemeRegistry schemeRegistry = new SchemeRegistry();
-		// XXX: very wierd - using deprecated constructor for Scheme it works. A
-		// suggested option
-		// - with factory and default port interchanged - fails. 10min debugging
-		// didn't result in
-		schemeRegistry.register(new Scheme("http", PlainSocketFactory
-				.getSocketFactory(), endpoint.getPort()));
-		schemeRegistry.register(new Scheme("https", blindTrustFactory, endpoint.getPort()));
+        SchemeRegistry schemeRegistry = new SchemeRegistry();
+        // XXX: very wierd - using deprecated constructor for Scheme it works. A
+        // suggested option
+        // - with factory and default port interchanged - fails. 10min debugging
+        // didn't result in
+        schemeRegistry.register(new Scheme("http", PlainSocketFactory
+                .getSocketFactory(), endpoint.getPort()));
+        schemeRegistry.register(new Scheme("https", blindTrustFactory, endpoint.getPort()));
 
-		ThreadSafeClientConnManager conMg = new ThreadSafeClientConnManager(
-				schemeRegistry);
-		conMg.setMaxTotal(2000);
-		conMg.setDefaultMaxPerRoute(200);
+        ThreadSafeClientConnManager conMg = new ThreadSafeClientConnManager(
+                schemeRegistry);
+        conMg.setMaxTotal(200);
+        conMg.setDefaultMaxPerRoute(10);
 
-		HttpHost host = new HttpHost(endpoint.getHost(), 80);
-		conMg.setMaxForRoute(new HttpRoute(host), 500);
+        HttpHost host = new HttpHost(endpoint.getHost(), 80);
+        conMg.setMaxForRoute(new HttpRoute(host), 20);
 
-		httpclient = new DefaultHttpClient(conMg);
+        httpclient = new DefaultHttpClient(conMg);
 
-		httpclient.getCredentialsProvider().setCredentials(
-				new AuthScope(endpoint.getHost(), endpoint.getPort()),
-				credentials);
+        httpclient.getCredentialsProvider().setCredentials(
+                new AuthScope(endpoint.getHost(), endpoint.getPort()),
+                credentials);
 
-		this.endpoint = endpoint;
+        this.endpoint = endpoint;
 
-		this.blobProxy = new BlobOperations(endpoint, httpclient);
-		this.containerProxy = new ContainerOperations(endpoint, httpclient);
-		this.nonCdmiContainerProxy = new NonCDMIContainerOperations(endpoint,
-				httpclient);
-		this.nonCdmiBlobProxy = new NonCDMIBlobOperations(endpoint, httpclient);
-	}
-	
-	public DefaultHttpClient getHttpclient() {
-		return httpclient;
-	}
+        this.blobProxy = new BlobOperations(endpoint, httpclient);
+        this.containerProxy = new ContainerOperations(endpoint, httpclient);
+        this.nonCdmiContainerProxy = new NonCDMIContainerOperations(endpoint,
+                httpclient);
+        this.nonCdmiBlobProxy = new NonCDMIBlobOperations(endpoint, httpclient);
+    }
 
-	public void setHttpclient(DefaultHttpClient httpclient) {
-		this.httpclient = httpclient;
-	}
+    public DefaultHttpClient getHttpclient() {
+        return httpclient;
+    }
 
-	public BlobOperations getBlobProxy() {
-		return blobProxy;
-	}
+    public void setHttpclient(DefaultHttpClient httpclient) {
+        this.httpclient = httpclient;
+    }
 
-	public void setBlobProxy(BlobOperations blobProxy) {
-		this.blobProxy = blobProxy;
-	}
+    public BlobOperations getBlobProxy() {
+        return blobProxy;
+    }
 
-	public ContainerOperations getContainerProxy() {
-		return containerProxy;
-	}
+    public void setBlobProxy(BlobOperations blobProxy) {
+        this.blobProxy = blobProxy;
+    }
 
-	public void setContainerProxy(ContainerOperations containerProxy) {
-		this.containerProxy = containerProxy;
-	}
+    public ContainerOperations getContainerProxy() {
+        return containerProxy;
+    }
 
-	public URL getEndpoint() {
-		return endpoint;
-	}
+    public void setContainerProxy(ContainerOperations containerProxy) {
+        this.containerProxy = containerProxy;
+    }
 
-	public void setEndpoint(URL endpoint) {
-		this.endpoint = endpoint;
-	}
+    public URL getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(URL endpoint) {
+        this.endpoint = endpoint;
+    }
 }
--- src/test/java/eu/venusc/cdmi/CDMIConnectionWrapper.java
+++ src/test/java/eu/venusc/cdmi/CDMIConnectionWrapper.java
@@ -29,7 +29,7 @@ public abstract class CDMIConnectionWrapper extends TestCase {
     }
 
     protected void createConnection() throws KeyManagementException, UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
-        cdmiServer = new URL("http://cdmi.pdc2.pdc.kth.se:2364/");
+        cdmiServer = new URL("http://localhost:2364/");
         parameters = new HashMap<String, Object>();
         credentials = new UsernamePasswordCredentials("christian", "venusc");
         cdmiConnection = new CDMIConnection(credentials, cdmiServer);
